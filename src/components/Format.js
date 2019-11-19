c890be3f35f6ebf8f69c589f07f1a10dd8cd440f
Reid Hanson <reidhans@buffalo.edu>
2019-11-19 10:57:18 -0500

    betters styling

--- src/components/Format.js
+++ src/components/Format.js
@@ -3,12 +3,14 @@ import React from 'react'
 const Format = props => {
   return (
     <li>
-      <img src={props.imageURL} alt={props.imageAlt} />
       <div>
+        <img src={props.imageURL} alt={props.imageAlt} />
+        {/* <div> */}
         <p>
           {props.catNumber}
           <span> {props.catID} </span>
         </p>
+        {/* </div> */}
       </div>
     </li>
   )
